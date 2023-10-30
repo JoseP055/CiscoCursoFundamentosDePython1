bb01b689a429e1b0faff410bdf5ea397e9e527aa
Jose Pablo <jospaba12@gmail.com>
2023-10-29 22:12:59 -0600

    Agregué la sección 2 del módulo 2

---
+++ PE1_Modulo2/2.2_Seccion2/2.2.6_Lab_LiteralesDePythonCadenas.py
@@ -0,0 +1,11 @@
+# LAB
+# Enunciado:
+#Escenario
+#Escriba un fragmento de código de una línea, utilizando la función print(), así como los caracteres de nuevalínea y de escape, para que coincida con el resultado esperado que se muestra en la salida.
+# Salida Esperada
+# "Estoy"
+# ""aprendiendo""
+# """Python"""
+
+print("\"Estoy\"\n\"\"Aprendiendo\"\"\n\"\"\"Python\"\"\"")
+#Logrado
