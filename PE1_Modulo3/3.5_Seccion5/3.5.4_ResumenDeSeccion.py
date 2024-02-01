99a04fb4c0507abfc748061c268e9ec808d08054
Jose Pablo <jospaba12@gmail.com>
2024-02-01 15:20:08 -0600

    Agregué la sección 5 del módulo 3

---
+++ PE1_Modulo3/3.5_Seccion5/3.5.4_ResumenDeSeccion.py
@@ -0,0 +1,17 @@
+#  Puedes usar el método sort() para ordenar los elementos de una lista, por ejemplo:
+
+
+lst = [5, 3, 1, 2, 4]
+print(lst)
+
+lst.sort()
+print(lst)  # output: [1, 2, 3, 4, 5]
+
+# 2.También hay un método de lista llamado reverse(), que puedes usar para invertir la lista, por ejemplo:
+
+
+lst = [5, 3, 1, 2, 4]
+print(lst)
+
+lst.reverse()
+print(lst)  # output: [4, 2, 1, 3, 5]
